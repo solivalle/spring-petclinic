4866e6589ab46eb349fc3d1e36d86f6dabdde08a
solivalle <127651078+solivalle@users.noreply.github.com>
2026-02-23 00:34:09 -0600

    Adding delivery 2: Include UTs for code coverage

---
+++ src/test/java/org/springframework/samples/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic;
+
+import org.junit.jupiter.api.Test;
+
+public class PetClinicApplicationTest {
+
+	@Test
+	void mainMethodRuns() {
+		PetClinicApplication.main(new String[] {});
+	}
+
+}
